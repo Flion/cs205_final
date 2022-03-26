1c3b6466120aba059baad9bda7f1a706047cab59
Maximilian Poole <maximilianpoole@Maximilians-MacBook-Pro.local>
2022-03-26 16:00:54 -0700

    changes to display the average HP and Defense stats of pokemon and count the total amount of non legendary pokemon

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -7,6 +7,8 @@
 #    Avg. Defense: [VALUE]
 # ======= END SUMMARY =======
 
+
+
 # NOTE THAT YOU MUST USE AWK OR YOU WILL LOSE POINTS
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The averages must only be for non-legendary pokemon.
@@ -15,3 +17,41 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+#change HP to add up all the HP values
+
+hp+=$6
+
+#add up the amount of defense stat
+
+defense+=$8
+
+#counts the non legendary pokemon
+if ($13 == "False"){
+
+   count+=1
+}
+
+}
+}
+END{
+#prints the display title
+print "======= SUMMARY OF POKEMON.DAT ======"
+
+#prints the amount of non legendary pokemon
+print "   Total Non-Legendary Pokemon: " count
+
+#prints the average HP of all pokemon
+print "   Avg. HP: " hp / 801
+
+#prints the average defense stat of the pokemon
+print "   Avg. Defense: " defense / 801
+
+# prints end
+print "======= END SUMMARY ======="}
+
