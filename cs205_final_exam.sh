8a19484a381aee02f683ca43b02bedd397f5eff6
gma246 <gma246@nau.edu>
2022-03-27 15:11:03 -0700

    Added new branch and edited cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,3 +15,24 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+FILE=$@
+total=0
+avg_hp=0
+avg_def=0
+
+if [ -f $FILE ]
+then
+	while read -r cur_line
+	do
+	if [ $3 != "Legendary" ]
+		total=$(( $total +1 ))
+		avg_hp=$(( $avg_hp + $5 ))
+		avg_def=$(( $avg_def +$7 ))
+	fi
+		echo $cur_line
+	done
+
+	echo "Total Non-Legendary Pokemon: " $total
+	echo "Avg. HP: " $avg_hp
+	echo "Avg. Defense: " $avg_def
+fi
