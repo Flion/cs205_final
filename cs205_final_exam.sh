4107c211167c549c9359c7dea09db0eb7834eaa3
Mumbi Macheho-Mbuthia <mm3569@nau.edu>
2022-03-26 22:19:53 -0700

    Changed .sh file according to instructions and added screenshot of output in new screenshots folder

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,38 @@
+#! /bin/awk -f
+# explanation: the line above signals this is an awk script
+
+# explanation: FS signifies the field separator character
+BEGIN{FS="\t"}
+{
+# explanation: skip the first line of the file since it contains the names of each column and not values
+if (NR!=1) {
+# explanation: if the value at the 13th column is False, it is not a legendary Pokemon
+if ($13=="False") {
+# explanation: add to the counter for total number of non-legendary Pokemon
+tnlp+=1
+# explanation: add to the hp counter
+hp+=$6
+# explanation: add to the defense counter
+def+=$8
+}
+}
+}
+# explanation: END block to be triggered once script has finished looping through file
+END{
+# explanation: print expected output as seen in instructions
+print "======= SUMMARY OF POKEMON.DAT ======"
+# explanation: print expected output as seen in instructions with total number of non-legendary Pokemon
+print "   Total Non-Legendary Pokemon: " tnlp
+# explanation: print expected output as seen in instructions with average number of HP for non-legendary Pokemon
+print "   Avg. HP: " hp/tnlp
+# explanation: print expected output as seen in instructions with average number of defense for non-legendary Pokemon
+print "   Avg. Defense: " def/tnlp
+# explanation: print expected output as seen in instructions
+print "======= END SUMMARY ======="
+}
+
+# ---
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file
 # formatted like pokemon.dat and provides a printed report in the following format (where your 
 # script correctly calculates the values that go into the [VALUE] placeholders):
