0b23b95b99aa4d25043a48bc3578a79920d6badb
kevkilbourne <kevkilbourne@gmail.com>
2022-03-26 18:35:06 -0700

    Updates made for functionality of the cs_205_final_exam script to the specifications of the TODO messaging

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,3 +15,36 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+# Kevin Kilbourne
+# CS 205
+# Final Exam Pt 2
+
+# grab our file from user input
+file=$1
+
+# if our file is true, roll with it
+if [[ -f $file ]];
+then
+
+	# grab the non-legendary pokemon in a separate transaction for better readability and producing a single error message
+	# in case of things going wrong
+	nonlegendary=`awk -F '\t' '/False/ {print}' $file`
+
+	# calculate our vars of interest
+	count=`echo "$nonlegendary" | awk -F '\t' 'END {print NR}'`
+	avgHP=`echo "$nonlegendary" | awk -F '\t' '{ sumHP += $5 } END { if (NR > 0) print sumHP / NR}'`
+	avgDF=`echo "$nonlegendary" | awk -F '\t' '{ sumDF += $7 } END { if (NR > 0) print sumDF / NR}'`
+
+	# print everything out all pretty-like
+	echo "======= SUMMARY OF POKEMON.DAT ======"
+	echo "Total Non-Legendary Pokemon: $count"
+	echo "Avg. HP: $avgHP"
+	echo "Avg. HP: $avgDF"
+	echo "======= END SUMMARY ======="
+
+else
+	# if the file doesn't exist, indicate this and exit
+	echo "File $file not found"
+
+fi
