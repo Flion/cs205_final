46078171ed0fc6a9747f285aa94d5c3f48aecf5b
Conrad-Murphy <chm73@nau.edu>
2022-03-27 13:45:01 -0700

    Modify script file, create and fill screenshots directory

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,3 +15,53 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+
+# Set file name variable
+file=$1
+
+# Output file text
+# Execute AWK using file name as a variable
+cat $file | awk -v file=$file '
+
+# Declare variables
+BEGIN{
+	# Set initial counters to 0
+	pokemonCount = 0
+	totalHP = 0
+	totalDefense = 0
+}
+
+# Execute for each line of data
+{
+	# Ignore header and legendaries
+	if (NR != 1 && $13 != "True") {
+
+		# Increment counter
+		pokemonCount++
+
+		# Increment HP and defense totals
+		totalHP += $6
+		totalDefense += $8
+	}
+}
+
+# Print data
+END {
+	# Print header
+	print "======= SUMMARY OF " toupper(file) " ======="
+	
+	# Print number of pokemon	
+	print "Total Non-Legendary Pokemon: " pokemonCount
+
+	# Print average HP
+	print "Avg. HP: " totalHP / pokemonCount
+
+	# Print average defense
+	print "Avg. Defense: " totalDefense / pokemonCount
+
+	# Print footer
+	print "======= END SUMMARY ======="
+}
+'
