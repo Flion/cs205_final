3a030f32bab8ee95fcac47a59859044ebd4f1fad
hypurrcat <koldkid3243@gmail.com>
2022-03-27 20:24:22 -0700

    I tried to edit pokemon.dat after pulling, but was unable to perform the specified operations due to how the file was formatted.

---
+++ djg386_spring2022/cs205_final_exam.sh
@@ -0,0 +1,22 @@
+# TODO: Modify this file to create a shell script that is able to use awk to go through a file
+# formatted like pokemon.dat and provides a printed report in the following format (where your 
+# script correctly calculates the values that go into the [VALUE] placeholders):
+# ======= SUMMARY OF POKEMON.DAT ======
+#    Total Non-Legendary Pokemon: [VALUE]
+#    Avg. HP: [VALUE]
+#    Avg. Defense: [VALUE]
+# ======= END SUMMARY =======
+
+# NOTE THAT YOU MUST USE AWK OR YOU WILL LOSE POINTS
+# The "Avg." values should be calculated as mean values for the corresponding columns.
+# The averages must only be for non-legendary pokemon.
+# The spacing and header formatting should match the above formatting description exactly.
+# There should be a comment explaining the purpose of each line in your shell script. 
+# The data file will be passed in to the script as a positional parameter and will not
+# necessarily be called pokemon.dat. However, you can assume that any file passed to this
+# script will be formatted exactly the way pokemon.dat is formatted.
+
+
+awk '{print $13}' < pokemon.dat
+# This is currently impossible to calculate because of the way the .dat file is formatted. Generation numbers as well as the wrong column header are included in column 13, which is the correct designation for legendary flags as shown in the .dat file on both github and locally (i.e. text editor and libre officewriter). Therfore, this calculation is not doable via means we've learned in this class. 
+# See the output for what I mean.
