c59f6d66079ad4b92226e4eaf0ea718cb696f705
miggd <91431104+miggd@users.noreply.github.com>
2022-03-27 23:36:34 -0700

    Update cs205_final_exam.sh

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,9 +15,19 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
-awk 'BEGIN{print "======== SUMMARY OF POKEMON.DAT ======="}; {print}; 
-
-
-
-
-END{print "======= END SUMMARY ======="}'
+if($7 = false){
+hp+=$6
+}
+#adds up total values in columm 6 if they're not legendary
+if ($7 = false){
+sum+=
+}
+#adds up total rows with false as legendary
+if($7 = false){
+sumdef+=$9
+}
+ ======= SUMMARY OF POKEMON.DAT ======
+    Total Non-Legendary Pokemon: print sum
+    Avg. HP: print hp/sum
+    Avg. Defense: print sumdef/sum
+ ======= END SUMMARY ======
