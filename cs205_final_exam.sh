56aca2ae5d989cdfdfa2550f964be4d34ebced2c
Kendall <krc498@nau.edu>
2022-03-27 22:17:14 -0700

    here is the second change made to my file

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,1 +1,4 @@
-1 I have created a new cs205 final instructions file due to the other not being able to be found
+1 I have created a new cs205 final instructions file due to the other not being able to be found 
+
+2 Here is my second change to the file 
+
