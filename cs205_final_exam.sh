a7a2fc04c7cadeafaa3405871962d5c487bfaa58
zakv <zvladich6564@gmail.com>
2022-03-27 19:04:50 -0700

    Created bash script to count non-legendary pokemon as well as average HP and defense. Screenshot added displaying expected output.

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file
 # formatted like pokemon.dat and provides a printed report in the following format (where your 
 # script correctly calculates the values that go into the [VALUE] placeholders):
@@ -15,3 +17,35 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+
+
+# Print Header using passed file path
+echo '======= SUMMARY '$1' ======'
+# Start Awk statement
+awk '
+# Split the file by tabs
+BEGIN{FS="\t"}
+{
+	# Exclude the header
+	if (NR!=1){
+		# Check that this pokemon is not legendary
+		if ($13 == "False"){
+			# Sum up the HP
+			hp += $6
+			# Sum up the Defense
+			def += $8
+			# Sum of non-lengendary
+			sum+=1
+		}
+	}
+}
+
+# Print out all of the totals
+END{
+	print "Total Non-Legendary Pokemon: " sum
+	print "Avg. HP: " hp/sum
+	print "Avg. Defense: " def/sum
+};
+' $1 # Use passed file path
+echo '======= END SUMMARY ======='
