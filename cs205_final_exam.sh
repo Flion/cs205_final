60614bfe31b7cc0235b4f312500b840791b3e7dd
cjb873 <cjb873@nau.edu>
2022-03-27 23:14:29 -0700

    updated bash script with awk and included a screenshot of output

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,3 +15,42 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+awk -f $1
+#begin awk script and look for tabs
+'BEGIN{FS="\t"}
+{
+# if not a legendary
+if($13!="true"){
+# add to hp total
+hp+=$6
+
+# add to defense total
+defense+=$8
+
+# add to total number of non legendaries
+sum+=1
+} # end of non legendary check
+} # end of file
+# do at end of file
+END
+{
+
+# avg hp
+hp/=sum
+
+# avg defense
+defense/=sum
+
+# print formatted header and sum
+printf "======= SUMMARY OF POKEMON.DAT ======\nTotal Non-Legendary Pokemon: %d", sum
+
+# print formatted hp avg
+printf "Avg. HP: %f", hp
+
+# print formatted defense avg
+printf "Avg. Defense: %f", defense
+
+# print end
+print "======= END SUMMARY ======="
+}' # end of awk script
