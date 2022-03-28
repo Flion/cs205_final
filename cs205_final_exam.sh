e77d6fb22f2e5e2571e01825c544576959217062
apf-nau <apf59@nau.edu>
2022-03-28 01:16:35 -0700

    added screenshot and modified script

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,3 +1,7 @@
+# Alexander Frenette
+# CS205 001
+
+
 # TODO: Modify this file to create a shell script that is able to use awk to go through a file
 # formatted like pokemon.dat and provides a printed report in the following format (where your 
 # script correctly calculates the values that go into the [VALUE] placeholders):
@@ -15,3 +19,20 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+# for all of the three lines below I am checking to make sure that we are not
+# looking at a legendary pokemon. from there I then take the information I am
+# interested in, the total numer seen, then the sum of all their hp, then the
+# sum of all their defense. I then go ahead and return the value from a
+# sub-shell. I do go ahead and make sure to use the first input arg as the arg
+# for awk as well.
+count=$(awk 'BEGIN {count=0;} { if(NR != 1 && $13 == "False") count++} END {print count}' $1)
+hp=$(awk 'BEGIN {hp=0;} { if(NR != 1 && $13 == "False") hp+=$5} END {print hp}' $1)
+def=$(awk 'BEGIN {def=0;} { if(NR != 1 && $13 == "False") def+=$7} END {print def}' $1)
+
+# display formatted data
+echo "======= SUMMARY OF POKEMON.DAT ======"
+echo "Total Non-Legendary Pokemon: $count"
+echo "Avg. HP: $((hp/count))"
+echo "Avg. Defense: $((def/count))"
+echo "======= END SUMMARY ======="
