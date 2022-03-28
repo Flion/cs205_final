7d30affbdb0e3cf002522537b2a45d022a58a78d
Stefan Mihailovic <night6srkn@gmail.com>
2022-03-28 06:06:45 +0000

    I modified the .sh file and made it calculate the number of nonlegendary pokemon, and calculate their average HP and Def stats

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -2,7 +2,7 @@
 # formatted like pokemon.dat and provides a printed report in the following format (where your 
 # script correctly calculates the values that go into the [VALUE] placeholders):
 # ======= SUMMARY OF POKEMON.DAT ======
-#    Total Non-Legendary Pokemon: [VALUE]
+#
 #    Avg. HP: [VALUE]
 #    Avg. Defense: [VALUE]
 # ======= END SUMMARY =======
@@ -15,3 +15,17 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+
+
+awk -v var="$1"
+#assigning the positional parameter to awk
+awk 'BEGIN {print " ======= SUMMARY OF POKEMON.DAT ======"}'
+awk 'BEGIN {FS="\t"} { if (NR!= 1 && $13=="False"){sum+=1}}END{print "    Total Non-Legendary Pokemon: "sum}' $1
+#calculating the number of nonlegendary pokmeon, 13th row
+awk 'BEGIN {FS="\t"} { if (NR!= 1 && $13=="False"){health += $6}{sum+=1}}END{print "    Avg. HP: "health/sum}' $1
+#calculating the average HP which is in 6th row
+awk 'BEGIN {FS="\t"} { if (NR!= 1 && $13=="False"){def += $8}{sum+=1}}END{print "    Avg. Defense: "def/sum}' $1
+#calculating the average defense which is in 8th row
+awk 'BEGIN {print " ======= END SUMMARY ======="}'
