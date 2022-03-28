ee85b32f3f20e03bf0257dd00149980012461ebd
Nicholas Wiley <nwiley333@gmail.com>
2022-03-28 06:42:46 +0000

    Made modifications to sc205_final_exam.sh so that it would create the desired output when run. A screenshot of the output was placed in the screenshots folder

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -15,3 +15,37 @@
 # The data file will be passed in to the script as a positional parameter and will not
 # necessarily be called pokemon.dat. However, you can assume that any file passed to this
 # script will be formatted exactly the way pokemon.dat is formatted.
+
+#to run as a bash script
+#!/bin/sh
+#so the script runs awk
+awk '
+#beginning the awk script and using tabs as delimiters
+BEGIN {FS="\t"}
+{
+	#Checking to see if the pokemon is legendary or not
+	if ($13 == "False"){
+		#adding non-legendary pokemon hp to total sum 
+		hp+=$5
+		#adding non-legendary pokemon def to total sum
+		def+=$7
+		#keeping track of the number of non-legendary pokemon for average
+		num_pokemon+=1
+	}
+}
+END {
+	#calculating avg hp
+	avg_hp = hp/num_pokemon
+	#calculating avg def
+	avg_def = def/num_pokemon
+	#formatting output
+	print "======= SUMMARY OF POKEMON.DAT ======\n"
+	#printing number of non-legendary pokemon
+	print "    Total Non-Legendary Pokemon:", num_pokemon, "\n"
+	#printing average hp
+	print "    Avg. HP:", avg_hp, "\n"
+	#printing avg def
+	print "    Avg. Defense:", avg_def, "\n"
+	#formatting output
+	print "======= END SUMMARY =======\n"
+} '
