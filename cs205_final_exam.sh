62655e8ead3c13c8d5608193f32f033f5dd242f6
Tomas <taj262@nau.edu>
2022-03-27 18:03:23 -0700

    I have add another counter for defence and use the same process as 1st mod to get the average then i created an if statment that checks all the legendsary and subtracts them by total amount

---
+++ cs205_final_exam.sh
@@ -0,0 +1,51 @@
+# 001
+# Jauregui
+# Tomas
+# taj262
+# TODO: Modify this file to create a shell script that is able to use awk to go through a file
+# formatted like pokemon.dat and provides a printed report in the following format (where your 
+# script correctly calculates the values that go into the [VALUE] placeholders):
+# ======= SUMMARY OF POKEMON.DAT ======
+#    Total Non-Legendary Pokemon: [VALUE]
+#    Avg. HP: [VALUE]
+#    Avg. Defense: [VALUE]
+# ======= END SUMMARY =======
+
+# NOTE THAT YOU MUST USE AWK OR YOU WILL LOSE POINTS
+# The "Avg." values should be calculated as mean values for the corresponding columns.
+# The averages must only be for non-legendary pokemon.
+# The spacing and header formatting should match the above formatting description exactly.
+# There should be a comment explaining the purpose of each line in your shell script. 
+# The data file will be passed in to the script as a positional parameter and will not
+# necessarily be called pokemon.dat. However, you can assume that any file passed to this
+# script will be formatted exactly the way pokemon.dat is formatted.
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+
+#TODO change HP to add up the number of HP on the Pokemon listed
+
+row+=1
+hp+= $5
+defense+= $7
+if ($12 == "True")
+then 
+legend=+1
+fi
+
+#Counts the number of lines in the file
+defsum = defense/row
+sum = hp / row
+total= row - legend
+}
+}
+END{
+print "======= SUMMARY OF POKEMON.DAT ======"
+print "Total Non-Legendary Pokemon: "
+print row - 26
+print   "Avg. HP:"
+print sum
+print  "Avg. Defense: "
+print defsum
+print"======= END SUMMARY ======="
+}
